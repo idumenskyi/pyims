b348fbd1b67e8f242aee86b633a8b945d873f371
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-22 22:03:30 +0300

    Update test_skype_send_message.py

--- test_skype_send_message.py
+++ test_skype_send_message.py
@@ -1,37 +1,40 @@
 import unittest
 import skype_send_message
+import keyring
+
+
 class skype_send_message_test(unittest.TestCase):
     """skype_send_message tests"""
+
     @classmethod
     def setUpClass(cls):
         """Set up for class"""
-        print ("setUpClass")
-        print ("==========")
+        print("setUpClass")
+        print("==========")
 
     @classmethod
     def tearDownClass(cls):
         """Tear down for class"""
-        print ("==========")
-        print ("tearDownClass")
+        print("==========")
+        print("tearDownClass")
 
     def setUp(self):
         """Set up for test"""
         print("Set up for [" + self.shortDescription() + "]")
+        print("password is set", keyring.set_password("Skype", "nexus12141", "Your Password"))
+
 
     def tearDown(self):
         """Tear down for test"""
-        print ("Tear down for [" + self.shortDescription() + "]")
-        print ("")
+        print("Tear down for [" + self.shortDescription() + "]")
+        print("")
+        print("password deleted: ", keyring.delete_password("Skype", "nexus12141"))
 
-    def test_password(self):
+    def test_password_from_keyring(self):
         """PASSWORD is SET, TEST"""
         print("id: " + self.id())
-        self.assertEqual(skype_send_message.keyring.get_password("Skype", "username"), "password")
+        self.assertEqual(skype_send_message.main("Skype", "nexus12141"), "Your Password")
 
-    def test_password1(self):
-        """PASSWORD is NOT SET, TEST"""
-        print("id: " + self.id())
-        self.assertNotEqual(skype_send_message.keyring.get_password("username", "password"), "password")
-        
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     unittest.main()
