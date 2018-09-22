e437d78685b3070b1ad83f095a1227a3ca8f7b1f
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-22 21:58:22 +0300

    Update skype_send_message.py

--- skype_send_message.py
+++ skype_send_message.py
@@ -5,23 +5,27 @@ parser = argparse.ArgumentParser()
 parent_parser = argparse.ArgumentParser(add_help=False)
 parent_parser.add_argument('--username', action="store")
 parent_parser.add_argument('--password', action="store")
-print("Welcome to 'Skype send message'. ")
-username = input("Type your username of Skype: ")
-password = keyring.get_password("Skype", username)
-if (password != keyring.get_password("Skype", username)):
-    password = input("Type your password of Skype: ")
+parent_parser.add_argument('--servicename', action="store")
+def main(servicename, username):
+    print("Welcome to 'Skype send message'. ")
+    password = keyring.get_password(servicename, username) or input("Type your password of Skype: ")
     try:
-        keyring.set_password("Skype", username,  password)
+        keyring.set_password(servicename, username,  password)
         print("password stored successfully")
     except keyring.errors.PasswordSetError:
         print("failed to store password")
-    print("password", keyring.get_password("Skype", username))
-contact = Skype(username, password)
-contact.user  # you
-contact.contacts  # your contacts
-contact.chats  # your conversations
-message = contact.contacts[input("Type user name of Skype for 1-to-1 conversation: ")].chat  # 1-to-1 conversation
-print("Sending: ")
-message.sendMsg(input("Type your message for user of Skype with you want to 1-to-1 conversation: "))
-message.getMsgs()  # retrieve recent messages
-print("Your message is send")
+        print("password", keyring.get_password(servicename, username))
+    contact = Skype(username, password)
+    contact.user  # you
+    contact.contacts  # your contacts
+    contact.chats  # your conversations
+    message = contact.contacts[input("Type user name of Skype for 1-to-1 conversation: ")].chat  # 1-to-1 conversation
+    print("Sending: ")
+    message.sendMsg(input("Type your message for user of Skype with you want to 1-to-1 conversation: "))
+    message.getMsgs()
+    print("Your message is send")
+    print(servicename, username, password)
+    return str(password)
+    pass
+if __name__ == '__main__':
+    main('servicename', 'username')
