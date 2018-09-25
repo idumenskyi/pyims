e95c9a1742237276e12e6ea92917310b2fcd8b01
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-25 13:12:20 +0300

    Update test_skype_send_message.py
    Send message from test.

--- test_skype_send_message.py
+++ test_skype_send_message.py
@@ -21,9 +21,9 @@ class skype_send_message_test(unittest.TestCase):
     def setUp(self):
         """Set up for test"""
         print("Set up for [" + self.shortDescription() + "]")
-        print("password is set", keyring.set_password("Skype", "nexus12141", "Your Password"))
-
-
+        print("password is set ", keyring.set_password("Skype", "nexus12141", "xxxxxxxx"))
+        print("password: ", keyring.get_password("Skype", "nexus12141"))
+        
     def tearDown(self):
         """Tear down for test"""
         print("Tear down for [" + self.shortDescription() + "]")
@@ -33,8 +33,9 @@ class skype_send_message_test(unittest.TestCase):
     def test_password_from_keyring(self):
         """PASSWORD is SET, TEST"""
         print("id: " + self.id())
-        self.assertEqual(skype_send_message.main("Skype", "nexus12141"), "Your Password")
+        self.assertEqual(skype_send_message.main("", "nexus12141", "xxxxxxxx", "nexus12142", "Hello, Test"), "Hello, Test")
 
 
 if __name__ == '__main__':
     unittest.main()
+
