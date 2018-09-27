6b103e7382795b564072d2f5188c4183a13a3322
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-27 19:19:57 +0300

    Update skype_send_message.py
    Updated with a note

--- skype_send_message.py
+++ skype_send_message.py
@@ -3,31 +3,29 @@ import argparse
 from skpy import Skype
 parser = argparse.ArgumentParser()
 parent_parser = argparse.ArgumentParser(add_help=False)
-parent_parser.add_argument('--username', action="store")
-parent_parser.add_argument('--password', action="store")
-parent_parser.add_argument('--servicename', action="store")
-parent_parser.add_argument('--user_conversation', action="store")
-parent_parser.add_argument('--message_conversation', action="store")
-def main(username, user_conversation, message_conversation):
+parent_parser.add_argument('--platform', action="store") # argument that save IM Platfform
+parent_parser.add_argument('--to_user', action="store")
+parent_parser.add_argument('--message_text', action="store")
+def main(username, to_user, message_text):
     print("Welcome to 'Skype send message'. ")
-    servicename = ""
-    password = keyring.get_password(servicename, username)
+    platform = ""
+    password = keyring.get_password(platform, username)
     try:
-        keyring.set_password(servicename, username,  password)
+        keyring.set_password(platform, username,  password)
         print("password stored successfully")
     except keyring.errors.PasswordSetError:
         print("failed to store password")
-        print("password", keyring.get_password(servicename, username))
+        print("password", keyring.get_password(platform, username))
     contact = Skype(username, password)
     contact.user  # you
     contact.contacts  # your contacts
     contact.chats  # your conversations
-    message = contact.contacts[user_conversation].chat  # 1-to-1 conversation
+    message = contact.contacts[to_user].chat  # 1-to-1 conversation
     print("Sending: ")
-    message.sendMsg(message_conversation)
+    message.sendMsg(message_text)
     message.getMsgs()
     print("Your message is send")
-    return str(message_conversation)
-    pass
+    return str(message_text)
+    
 if __name__ == '__main__':
-    main('user_conversation', 'message_conversation')
+    main('to_user', 'message_text')
