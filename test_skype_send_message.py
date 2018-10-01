af4dca2c7c531d6d2f30548ee8efa141396fecf1
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-10-01 21:51:47 +0300

    Update test_skype_send_message.py
    Added global variable SKYPE_KEYRING, delete value of argument platform from test

--- test_skype_send_message.py
+++ test_skype_send_message.py
@@ -28,12 +28,12 @@ class SkypeSendTextMessageTest(unittest.TestCase):
         """Tear down for test"""
         print("Tear down for [" + self.shortDescription() + "]")
         print("")
-        print("password deleted: ", keyring.delete_password("Skype", "nexus12141"))
+        print("password deleted: ", keyring.delete_password(SKYPE_KEYRING, "nexus12141"))
 
     def test_send_text_message(self):
         """SEND MESSAGE is SET, TEST"""
         print("id: " + self.id())
-        self.assertEqual(skype_send_message.main("Skype", "nexus12141", "nexus12142", "HELLO, TEST"), "HELLO, TEST")
+        self.assertEqual(skype_send_message.main("nexus12141", "nexus12142", "HELLO, TEST"), "HELLO, TEST")
 
 
 if __name__ == '__main__':
