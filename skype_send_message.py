2f2f8deeb354b4e28219e382231e8dcbf3f25f3a
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-10-01 21:56:50 +0300

    Update skype_send_message.py
    Added global variable SKYPE_KEYRING, delete argument platform from main()

--- skype_send_message.py
+++ skype_send_message.py
@@ -1,14 +1,15 @@
 import keyring
 import argparse
 from skpy import Skype
+SKYPE_KEYRING = 'ims_skype_keyring'
 parser = argparse.ArgumentParser()
 parent_parser = argparse.ArgumentParser(add_help=False)
-parent_parser.add_argument('--platform', action="store") # argument that save IM Platfform
+parent_parser.add_argument('--SKYPE_KEYRING', action="store")  # argument that save IM Platfform
 parent_parser.add_argument('--to_user', action="store")
 parent_parser.add_argument('--message_text', action="store")
-def main(platform, username, to_user, message_text):
+def main(username, to_user, message_text):
     print("Welcome to 'Skype send message'. ")
-    password = keyring.get_password(platform, username)
+    password = keyring.get_password(SKYPE_KEYRING, username)
     contact = Skype(username, password)
     contact.user  # you
     contact.contacts  # your contacts
