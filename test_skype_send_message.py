0cbcfa0823216b0f027500098fa8c968a0d646f9
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-10-08 16:00:25 +0300

    Update test_skype_send_message.py

--- test_skype_send_message.py
+++ test_skype_send_message.py
@@ -22,24 +22,24 @@ class SkypeSendTextMessageTest(unittest.TestCase):
         """Set up for test"""
         print("Set up for [" + self.shortDescription() + "]")
         #print("password is set ", keyring.set_password(SKYPE_KEYRING, "nexus12141", "xxxxxxxx"))
-        print("password is set ", keyring.set_password(SKYPE_KEYRING, "nexus12142", "xxxxxxxx"))
+        print("password is set ", keyring.set_password(SKYPE_KEYRING, "nexus12141", "xxxxxxxx"))
         
         
     def tearDown(self):
         """Tear down for test"""
         print("Tear down for [" + self.shortDescription() + "]")
         print("")
-        print("password deleted: ", keyring.delete_password(SKYPE_KEYRING, "nexus12142"))
+        print("password deleted: ", keyring.delete_password(SKYPE_KEYRING, "nexus12141"))
 
     def test_send_text_message(self):
         """SEND MESSAGE is SET, TEST"""
         print("id: " + self.id())
-        self.assertEqual(skype_send_message.main("nexus12142", "nexus12141", "HELLO, TEST"), "HELLO, TEST")
+        self.assertEqual(skype_send_message.main("nexus12141", "nexus12142", "HELLO, TEST"), "HELLO, TEST")
  
     def test_messages_text_message(self):
         """MESSAGES"""
         print("id: " + self.id())
-        self.assertEqual(skype_send_message.messages("nexus12142", "nexus12141", "HELLO, TEST"), "HELLO, TEST")
+        self.assertEqual(skype_send_message.messages("nexus12141", "nexus12142", "HELLO, TEST"), "HELLO, TEST")
 
 if __name__ == '__main__':
     unittest.main()
