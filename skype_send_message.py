cbb1573cd47c620e97a2802416953a6bf1da0493
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-10-10 18:11:32 +0300

    Update skype_send_message.py
    Updated

--- skype_send_message.py
+++ skype_send_message.py
@@ -2,6 +2,7 @@ import keyring
 import argparse
 from skpy import Skype
 SKYPE_KEYRING = 'ims_skype_keyring'
+global message_text
 parser = argparse.ArgumentParser()
 parent_parser = argparse.ArgumentParser(add_help=False)
 parent_parser.add_argument('--SKYPE_KEYRING', action="store")  # argument that save IM Platfform
@@ -18,9 +19,10 @@ def main(username, to_user, message_text):
     print("Sending: ")
     message.sendMsg(message_text)
     message.getMsgs()
-    print("Your message is send")
-    return str(message_text)
-
+    mess = message.getMsgs()
+    rms = mess[0].content
+    print(rms)
+    return str(rms)
 
 def messages(username, from_username):
     password = keyring.get_password(SKYPE_KEYRING, username)
@@ -28,10 +30,11 @@ def messages(username, from_username):
     contact.user  # you
     contact.contacts  # your contacts
     contact.chats  # your conversations
-    message = contact.contacts[username].chat
-    print(message_text)
-    return str(message_text)
-
+    message = contact.contacts[from_username].chat  # 1-to-1 conversation
+    message.getMsgs()
+    mess = message.getMsgs()
+    rms = mess[0].content
+    return str(rms)
 
 if __name__ == '__main__':
     main('username', 'to_user', 'message_text')
