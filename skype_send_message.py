17d7e4160c9a2ec1c22cb218c0547bec35e8d398
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2019-01-11 14:16:51 +0200

    Update skype_send_message.py

--- skype_send_message.py
+++ skype_send_message.py
@@ -1,6 +1,6 @@
 from skpy import Skype
-sk = Skype("nexus12141", "2202414q") # connect to Skype
-ch = sk.contacts["nexus12142"].chat # 1-to-1 conversation
+sk = Skype("YOUR SKYPE USERNAME", "YOU SKYPE PASSWORD") # connect to Skype
+ch = sk.contacts["SKYPE USERNAME FOR CONVERSATION"].chat # 1-to-1 conversation
 mess = ch.sendMsg("HI TEST")
 rms = ch.getMsgs()
 print(rms)
